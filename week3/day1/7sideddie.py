9873e3f2a060e2a0d4f77cbb0db1eea484851e9f
sharanyamarineni <sharanya.marineni@gmail.com>
2018-07-10 04:54:11 -0400

    Create 7sideddie.py

---
+++ week3/day1/7sideddie.py
@@ -0,0 +1,19 @@
+import random
+
+
+def rand5():
+    return random.randint(1, 5)
+
+
+def random7():
+    return 5 * rand5() + rand5() - 5
+def rand7():
+    while True:
+        r = random7()
+        if r < 22:
+            return (r%7+1)
+    return 0
+
+
+print 'Rolling 7-sided die...'
+print rand7()
